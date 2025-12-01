xconnio/deskconn-backend

add database models

deskconn/database/database.py
@@ -1,0 +1,21 @@
+import os
+
+from dotenv import load_dotenv
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+
+from deskconn.models import Base
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DESKCONN_DSN", None)
+if DATABASE_URL is None or DATABASE_URL == "":
+    raise ValueError("'DESKCONN_DSN' missing in environment variables.")
+
+engine = create_async_engine(DATABASE_URL, echo=True)
+
+AsyncSessionLocal = async_sessionmaker(engine, expire_on_commit=False, autoflush=False, autocommit=False)
+
+
+async def init_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)

deskconn/models.py
@@ -1,0 +1,58 @@
+from datetime import datetime, timezone
+import enum
+
+from sqlalchemy import Enum
+from sqlalchemy import Integer, ForeignKey, Text, DateTime
+from sqlalchemy.orm import relationship, declarative_base, mapped_column
+
+Base = declarative_base()
+
+
+def utcnow():
+    return datetime.now(timezone.utc)
+
+
+class UserRole(str, enum.Enum):
+    guest = "guest"
+    user = "user"
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = mapped_column(Integer, primary_key=True)
+    email = mapped_column(Text, unique=True, nullable=False)
+    password = mapped_column(Text, nullable=False)
+    name = mapped_column(Text, nullable=False)
+    role = mapped_column(Enum(UserRole, name="user_role"), nullable=False, default=UserRole.guest)
+
+    created_at = mapped_column(DateTime(timezone=True), default=utcnow)
+
+    devices = relationship("Device", back_populates="user", cascade="all, delete-orphan", passive_deletes=True)
+
+
+class Device(Base):
+    __tablename__ = "devices"
+
+    id = mapped_column(Integer, primary_key=True)
+    device_id = mapped_column(Text, unique=True, nullable=False)
+    name = mapped_column(Text)
+
+    created_at = mapped_column(DateTime(timezone=True), default=utcnow)
+
+    user_id = mapped_column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False, index=True)
+    user = relationship("User", back_populates="devices")
+
+    device_keys = relationship("DeviceKey", back_populates="device", cascade="all, delete-orphan", passive_deletes=True)
+
+
+class DeviceKey(Base):
+    __tablename__ = "device_keys"
+
+    id = mapped_column(Integer, primary_key=True)
+    public_key = mapped_column(Text, nullable=False, index=True)
+
+    created_at = mapped_column(DateTime(timezone=True), default=utcnow)
+
+    device_id = mapped_column(Integer, ForeignKey("devices.id", ondelete="CASCADE"), nullable=False, index=True)
+    device = relationship("Device", back_populates="device_keys")

xconnio/deskconn-account-service

deskconn/database/database.py
@@ -1,0 +1,23 @@
+import os
+
+from dotenv import load_dotenv
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+
+from deskconn.models import Base
+
+load_dotenv()
+
+DATABASE_PATH = os.getenv("DESKCONN_DBPATH", None)
+if DATABASE_PATH is None or DATABASE_PATH == "":
+    raise ValueError("'DESKCONN_DBPATH' missing in environment variables.")
+
+DATABASE_URL = f"sqlite+aiosqlite:///{DATABASE_PATH}"
+
+engine = create_async_engine(DATABASE_URL, echo=True)
+
+AsyncSessionLocal = async_sessionmaker(engine, expire_on_commit=False, autoflush=False, autocommit=False)
+
+
+async def init_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)

deskconn/models.py
@@ -1,0 +1,64 @@
+import enum
+from datetime import datetime, timezone
+
+from sqlalchemy import Enum, Integer, ForeignKey, Text, DateTime
+from sqlalchemy.orm import relationship, declarative_base, mapped_column
+
+Base = declarative_base()
+
+
+def utcnow():
+    return datetime.now(timezone.utc)
+
+
+class UserRole(str, enum.Enum):
+    guest = "guest"
+    user = "user"
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = mapped_column(Integer, primary_key=True)
+    email = mapped_column(Text, unique=True, nullable=False)
+    password = mapped_column(Text, nullable=False)
+    name = mapped_column(Text, nullable=False)
+    role = mapped_column(Enum(UserRole, name="user_role"), nullable=False, default=UserRole.guest)
+
+    created_at = mapped_column(DateTime(timezone=True), default=utcnow)
+
+    devices = relationship("Device", back_populates="user", cascade="all, delete-orphan", passive_deletes=True)
+    desktops = relationship("Desktop", back_populates="user", cascade="all, delete-orphan", passive_deletes=True)
+
+
+class Device(Base):
+    __tablename__ = "devices"
+
+    id = mapped_column(Integer, primary_key=True)
+    device_id = mapped_column(Text, unique=True, nullable=False)
+    name = mapped_column(Text)
+    public_key = mapped_column(Text, nullable=False, index=True)
+
+    created_at = mapped_column(DateTime(timezone=True), default=utcnow)
+
+    user_id = mapped_column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False, index=True)
+
+    user = relationship("User", back_populates="devices", cascade="all, delete-orphan", passive_deletes=True)
+    desktops = relationship("Desktop", back_populates="device", cascade="all, delete-orphan", passive_deletes=True)
+
+
+class Desktop(Base):
+    __tablename__ = "desktops"
+
+    id = mapped_column(Integer, primary_key=True)
+    desktop_id = mapped_column(Text, unique=True, nullable=False)
+    name = mapped_column(Text)
+    public_key = mapped_column(Text, nullable=False, index=True)
+
+    created_at = mapped_column(DateTime(timezone=True), default=utcnow)
+
+    user_id = mapped_column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False, index=True)
+    user = relationship("User", back_populates="desktops", cascade="all, delete-orphan", passive_deletes=True)
+
+    device_id = mapped_column(Integer, ForeignKey("devices.id", ondelete="CASCADE"), nullable=False, index=True)
+    device = relationship("Device", back_populates="devices", cascade="all, delete-orphan", passive_deletes=True)